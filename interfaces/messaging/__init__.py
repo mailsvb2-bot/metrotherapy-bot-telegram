21c386fc4919ff60f501a4b1de607f23a55ec543
BusinesMetro <mailsvb2@gmail.com>
2026-05-27 11:15:38 +0300

    Add messaging interface package

---
+++ interfaces/messaging/__init__.py
@@ -0,0 +1,8 @@
+from interfaces.messaging.contracts import MessengerButton, MessengerMessage, MessengerPlatform, normalize_platform
+
+__all__ = [
+    "MessengerButton",
+    "MessengerMessage",
+    "MessengerPlatform",
+    "normalize_platform",
+]
