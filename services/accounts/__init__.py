3b4fd9658f147683ee95c4f2923bd68185cd4c76
BusinesMetro <mailsvb2@gmail.com>
2026-07-02 01:11:58 +0300

    Export account merge service

--- services/accounts/__init__.py
+++ services/accounts/__init__.py
@@ -5,9 +5,13 @@ from services.accounts.identity import (
     link_channel_to_account,
     resolve_account_for_identity,
 )
+from services.accounts.merge import AccountMergePlan, apply_account_merge, build_account_merge_plan
 
 __all__ = [
     "AccountIdentityConflict",
+    "AccountMergePlan",
+    "apply_account_merge",
+    "build_account_merge_plan",
     "ensure_account",
     "get_account_snapshot",
     "link_channel_to_account",
